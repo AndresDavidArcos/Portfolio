96c03c39198a2a4bfa3b930e68daee0b3820441c
AndresDavidArcos <andres.camargo@correounivalle.edu.co>
2023-08-09 15:35:31 -0500

    es cine

---
+++ spyScroll.js
@@ -0,0 +1,22 @@
+(d => {
+    const $mainSections = d.querySelectorAll(".mainSection");
+    
+    const sectionsObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const sectionName = entry.target.getAttribute("id");
+            const $actualLink = d.querySelector(`.menuLinks a[href = '#${sectionName}']`),
+            $actualIcon = d.querySelector(`.icon-${sectionName}`);
+
+            $actualLink.classList.toggle("menuLinkObserved", entry.isIntersecting);
+            $actualIcon.classList.toggle("elementHidden", !entry.isIntersecting);
+
+        })
+
+    }, {
+        threshold: 0.6
+    })
+
+    $mainSections.forEach(section => {
+       sectionsObserver.observe(section);
+    })
+})(document)
